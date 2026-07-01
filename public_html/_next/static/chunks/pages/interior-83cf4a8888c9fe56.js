708f9c9ed15395b2bcdb85b43bf138af98456ab3
AntSavva <AntSavva@users.noreply.github.com>
2026-07-01 10:39:55 +0300

    Remove unused Next JS files from PHP bundle

--- public_html/_next/static/chunks/pages/interior-83cf4a8888c9fe56.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[152],{982:(_,n,u)=>{"use strict";u.r(n),u.d(n,{__N_SSP:()=>s,default:()=>t});var e=u(7876),r=u(7581),s=!0;function t(_){return(0,e.jsx)(r.A,{..._})}},5214:(_,n,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/interior",function(){return u(982)}])}},_=>{_.O(0,[986,249,636,593,792],()=>_(_.s=5214)),_N_E=_.O()}]);
